2d102a2a291faede42f4adfdafecc2ff96573831
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-10-15 20:35:00 +0700

    change iamge avatar

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,5 +1,5 @@
 // images.js
 export const IMAGES = {
     HERO: new URL('./totnghiep.png', import.meta.url).href,
-    AVATAR: new URL('./avatar.png', import.meta.url).href,
+    AVATAR: new URL('./avatar.jpg', import.meta.url).href,
 };
