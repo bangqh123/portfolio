6f26611ae3efcb9060ec3ed9b55d14b88ad359ae
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-10-26 23:46:15 +0700

    add icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,5 @@
 export const ICONS = {
     HI: new URL('./Hi.svg', import.meta.url).href,
+    CLOSE: new URL('./close.svg', import.meta.url).href,
+    HAMBURGER: new URL('./hamburger.svg', import.meta.url).href,
 };
