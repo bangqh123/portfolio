9e62034c90d4bdee96dabe8786620dd07fbd1fc7
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-11-03 17:58:20 +0700

    create skill

--- src/pages/home/partials/Skill/Skill.tsx
+++ src/pages/home/partials/Skill/Skill.tsx
@@ -1,9 +1,14 @@
-import './Skill.scss'
+import "./Skill.scss"
 
 const Skill = () => {
     return (
         <div id="skill" className="skill">
           <h1 className='skill-title'>Skill</h1>
+          <div className="skill-content">
+            <div className="skill-content-language"></div>
+            <div className="skill-content-tool"></div>
+            <div className="skill-content-chart"></div>
+          </div>
         </div>
       );
 }
