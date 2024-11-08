8a37a9f7d99b220e22d4deea68cef69e519efc44
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-11-08 21:17:26 +0700

    add resume

--- src/pages/home/partials/Resume/Resume.tsx
+++ src/pages/home/partials/Resume/Resume.tsx
@@ -1,14 +1,11 @@
 import "./Resume.scss"
+import "./Resume.media.scss"
 
 const Resume = () => {
     return (
         <div id="resume" className="resume">
           <h1 className='resume-title'>Resume</h1>
-          {/* <div className="resume-content">
-            <div className="resume-content-language"></div>
-            <div className="resume-content-tool"></div>
-            <div className="resume-content-chart"></div>
-          </div> */}
+          <object className="resume-cv" data="/QH_Bang_FrontEnd.pdf" type="application/pdf" />
         </div>
       );
 }
