cbe6c6a19722c24a70b8f81a8be2ff3151b6712e
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-11-09 01:52:46 +0700

    create loading

---
+++ src/components/loadings/Loadings.tsx
@@ -0,0 +1,25 @@
+import "./Loadings.scss";
+
+import React, { useEffect, useState } from "react";
+import { RingLoader } from "react-spinners";
+
+type LoadingProps = {
+  isLoading: boolean
+};
+
+const Loading: React.FC<LoadingProps> = ( { isLoading } ) => {
+  const [loading, setLoading] = useState(isLoading)
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, [])
+
+  return (
+    <div className="loading-container">
+      <RingLoader size={80} color="white" loading={loading} />
+    </div>
+  );
+};
+
+export default Loading;
