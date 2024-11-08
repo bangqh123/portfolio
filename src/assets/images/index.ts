9fa12327739e938698849552adf016fcb09096bf
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-11-09 00:04:48 +0700

    create not found page

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -2,4 +2,5 @@
 export const IMAGES = {
     HERO: new URL('./DSC02326.png', import.meta.url).href,
     AVATAR: new URL('./DSC02313.png', import.meta.url).href,
+    ERROR: new URL('./404.png', import.meta.url).href
 };
