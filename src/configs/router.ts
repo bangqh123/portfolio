0a490d1bd7b8a667b908736a85cadf3609538f48
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-11-09 01:54:07 +0700

    add page

--- src/configs/router.ts
+++ src/configs/router.ts
@@ -2,8 +2,14 @@ interface IRoute {
   [key: string]: string;
 
   HOMEPAGE: string;
+  RESUMEPAGE: string;
+  PROJECTPAGE: string;
+  CONTACTPAGE: string;
 }
 
 export const ROUTES: IRoute = {
   HOMEPAGE: "",
+  RESUMEPAGE: "/resume",
+  PROJECTPAGE: "/project",
+  CONTACTPAGE: "/contact",
 };
