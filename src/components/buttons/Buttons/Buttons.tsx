bdcf086e35271a0e88d0537e5b48f28ca6e99e69
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-11-10 17:27:09 +0700

    update buttons

--- src/components/buttons/Buttons/Buttons.tsx
+++ src/components/buttons/Buttons/Buttons.tsx
@@ -7,23 +7,17 @@ interface TButtonsProps {
   title?: string;
   className?: string;
   icon?: React.ReactNode;
-  download?: boolean;
   newTab?: boolean;
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const Buttons: React.FC<TButtonsProps> = ({ title, className, icon, link, download, newTab, onClick }) => {
+const Buttons: React.FC<TButtonsProps> = ({ title, className, icon, link, newTab, onClick }) => {
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     if (onClick) {
       onClick(e);
     } else if (link) {
       if (newTab) {
         window.open(link, "_blank");
-      } else if (download) {
-        const linkElement = document.createElement("a");
-        linkElement.href = link;
-        linkElement.download = "CV_Quach_Hai_Bang.pdf";
-        linkElement.click();
       } else {
         window.location.href = link;
       }
