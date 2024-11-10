30dd1b3fd39ed46fb93627da2be0e29a5818e93e
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-11-10 17:26:52 +0700

    change

---
+++ src/pages/contact/ContactPage.tsx
@@ -0,0 +1,18 @@
+import "./ContactPage.scss"
+
+import LayoutPage from "../../layouts/LayoutPage/LayoutPage";
+import ContactForm from "./partials/ContactForm/ContactForm";
+import ContactMap from "./partials/ContactMap/ContactMap";
+
+const ContactPage = () => {
+    return (
+      <LayoutPage>
+        <div className="contact">
+          <ContactMap />
+          <ContactForm />
+        </div>
+      </LayoutPage>
+    )
+  };
+  
+  export default ContactPage;
---
+++ src/pages/resume/ResumePage.tsx
@@ -0,0 +1,12 @@
+import LayoutPage from "../../layouts/LayoutPage/LayoutPage";
+import ResumeItem from "./partials/ResumeItem/ResumeItem";
+
+const ResumePage = () => {
+  return (
+    <LayoutPage>
+      <ResumeItem />
+    </LayoutPage>
+  )
+};
+
+export default ResumePage;
