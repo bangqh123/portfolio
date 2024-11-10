11614cde983d722f5221f32b966d0096d067d5c3
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-11-10 17:25:32 +0700

    add image project shop

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -2,5 +2,6 @@
 export const IMAGES = {
     HERO: new URL('./hero.png', import.meta.url).href,
     AVATAR: new URL('./about.png', import.meta.url).href,
-    ERROR: new URL('./404.png', import.meta.url).href
+    ERROR: new URL('./404.png', import.meta.url).href,
+    SHOP: new URL('./Shop.png', import.meta.url).href,
 };
