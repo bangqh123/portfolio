106f3b5343d611a41cf2ab03fead5ea1afe7d970
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-11-10 17:28:06 +0700

    create props project

---
+++ src/configs/constants/projectpage.ts
@@ -0,0 +1,10 @@
+const projectsProps = {
+    title: 'My Projects',
+    description: 'Here are a few projects I have worked on recently',
+}
+
+export const PROPS = {
+    PROJECT: {
+        ...projectsProps
+    }
+}
