7915ae3c644b7e2b1c0ff087185244a4269bb023
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-11-12 00:02:32 +0700

    render data

--- src/pages/home/partials/Educations/Education.tsx
+++ src/pages/home/partials/Educations/Education.tsx
@@ -1,39 +1,74 @@
-import "./Education.scss"
-import "./Education.media.scss"
+import "./Education.scss";
+import "./Education.media.scss";
 
-import React from "react"
+import React, { useEffect, useState } from "react";
+import { RootState, AppDispatch } from "../../../../redux/store/store";
+import { fetchPortfolio } from "../../../../redux/slices/portfolioSlices";
+import { useDispatch, useSelector } from "react-redux";
 
 type TEducationProps = {
-  title: string,
-  schoolName: string,
-  major: string,
-  subjects: string,
-  gpa: string,
-  start: string,
-  end: string,
+  title: string;
+  schoolName: string;
+  major: string;
+  subjects: string;
+  gpa: string;
+  start: string;
+  end: string;
+};
+
+interface TEducation {
+  id: number;
+  school_name: string;
+  major: string;
+  desc: string;
+  gpa: string;
+  start: string;
+  end: string;
 }
 
-const Education: React.FC<TEducationProps> = ({title, schoolName, major, subjects,gpa, start,end}) => {
-    return (
-        <div id="education" className="education">
-          <h1 className='education-title'>{title}</h1>
-          <div className='education-school'>
-            <div className='education-school-item'>
-              <div className='school-item-infor'>
-                <h2 className='item-infor-name'>{schoolName}</h2>
-                <h3 className='item-infor-major'>{major}</h3>
-                <p className='item-infor-subject'>{subjects}</p>
-                <span className='item-infor-gpa'>{gpa}</span>
-              </div>
-              <div className='school-item-time'>
-                <p className='item-time-start'>{start}</p>
-                <span>-</span>
-                <p className='item-time-end'>{end}</p>
-              </div>
+const Education: React.FC<TEducationProps> = ({ title, major, gpa }) => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  const [isData, setIsData] = useState<TEducation[] | null>(null);
+
+  const { userInfo } = useSelector((state: RootState) => state.portfolio);
+
+  useEffect(() => {
+    if (!userInfo?.[0]) {
+      dispatch(fetchPortfolio());
+    } else {
+      setIsData(userInfo?.[0].education);
+    }
+  }, [dispatch, userInfo]);
+
+  return (
+    <div id="education" className="education">
+      <h1 className="education-title">{title}</h1>
+      <div className="education-school">
+        {isData?.map((item, index) => (
+          <div key={index} className="education-school-item">
+            <div className="school-item-infor">
+              <h2 className="item-infor-name">{item.school_name}</h2>
+              <h3 className="item-infor-major">
+                {major}
+                {item.major}
+              </h3>
+              <p className="item-infor-subject">{item.desc}</p>
+              <span className="item-infor-gpa">
+                {gpa}
+                {item.gpa}
+              </span>
+            </div>
+            <div className="school-item-time">
+              <p className="item-time-start">{item.start}</p>
+              <span>-</span>
+              <p className="item-time-end">{item.end}</p>
             </div>
           </div>
-        </div>
-      );
-}
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Education;
