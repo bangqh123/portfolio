f0de80ee7acca4ea3928b6460896832064e001b0
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-11-12 16:27:24 +0700

    get data

--- src/pages/resume/partials/ResumeItem/ResumeItem.tsx
+++ src/pages/resume/partials/ResumeItem/ResumeItem.tsx
@@ -1,14 +1,32 @@
 import "./ResumeItem.scss"
 import "./ResumeItem.media.scss"
 
-import CV from "../../../../assets/file/QH_Bang_FrontEnd.pdf"
+import React, { useEffect, useState } from "react";
+import { RootState, AppDispatch } from "../../../../redux/store/store";
+import { useDispatch, useSelector } from "react-redux";
 
-const ResumeItem = () => {
+interface TResumeItem {
+  cv: string
+}
+
+const ResumeItem: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  const [isData, setIsData] = useState<TResumeItem>();
+
+  const { userInfo } = useSelector((state: RootState) => state.portfolio);
+
+  useEffect(() => {
+    setIsData(userInfo?.[0].resume);
+  }, [dispatch, userInfo]);
+
+  console.log(isData)
+
     return (
       <div className="resumeitem">
         <object 
           className="resumeitem-cv" 
-          data={CV}
+          data={isData?.cv} 
           type="application/pdf" 
         />
       </div>
