0da41075c8a26f6023e959991d8a8e22ba0df27a
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-11-12 17:24:57 +0700

    get data project

--- src/pages/projects/ProjectsPage.tsx
+++ src/pages/projects/ProjectsPage.tsx
@@ -1,8 +1,25 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProjects } from "../../redux/slices/projectsSlice";
+import { RootState, AppDispatch } from "../../redux/store/store";
+
 import LayoutPage from "../../layouts/LayoutPage/LayoutPage";
 import Project from "./partials/Projects/Projects";
 import { PROPS } from "../../configs/constants/projectpage";
 
 const ProjectPage = () => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  const { projectInfo } = useSelector(
+    (state: RootState) => state.project
+  );
+
+  useEffect(() => {
+    if (!projectInfo) {
+      dispatch(fetchProjects());
+    }
+  }, [dispatch, projectInfo]);
+
   return (
     <LayoutPage>
       <Project {...PROPS.PROJECT}/>
