3fc3697d77eaab7e19830b620cd862167d9689c1
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-11-12 19:14:43 +0700

    notification

--- src/components/notifications/notifications.tsx
+++ src/components/notifications/notifications.tsx
@@ -1,15 +1,43 @@
-import { ToastContainer, toast } from "react-toastify";
+import "./notification.scss"
+
+import React from "react";
+import { Flip, ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const Notify = () => {
-  const notify = () => toast("Wow so easy!");
+type TNotifyProps = {
+  status: "success" | "error";
+  message: string;
+};
+
+const Notify: React.FC<TNotifyProps> = ({ status, message }) => {
+  const showToast = () => {
+    switch (status) {
+      case "success":
+        toast.success(message);
+        break;
+      default:
+        toast.error(message); 
+        break;
+    }
+  };
+
+  React.useEffect(() => {
+    showToast();
+  }, [status, message]);
 
   return (
     <div>
-      <button onClick={notify}>Notify!</button>
-      <ToastContainer />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+        transition={Flip}
+      />
     </div>
   );
-}
+};
 
-export default Notify
+export default Notify;
