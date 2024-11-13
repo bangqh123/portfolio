8e510312b67709f885a61f71f5d28dfe01b7c9d1
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-11-13 21:26:46 +0700

    update code

--- src/components/buttons/Buttons/Buttons.tsx
+++ src/components/buttons/Buttons/Buttons.tsx
@@ -11,18 +11,25 @@ interface TButtonsProps {
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const Buttons: React.FC<TButtonsProps> = ({ title, className, icon, link, newTab, onClick }) => {
+const Buttons: React.FC<TButtonsProps> = ({
+  title,
+  className,
+  icon,
+  link,
+  newTab,
+  onClick,
+}) => {
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     if (onClick) {
       onClick(e);
     } else if (link) {
-      if (newTab) {
+      if (newTab === true) {
         window.open(link, "_blank");
       } else {
         window.location.href = link;
       }
     }
-  };
+  };  
 
   return (
     <button className={`button ${className}`} onClick={handleClick}>
