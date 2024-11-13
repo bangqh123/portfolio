0bb7f4e0dc0f01ad581701347ee07128134ff29f
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-11-13 21:26:33 +0700

    update path

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,5 +1,4 @@
 export const ICONS = {
-    HI: new URL('./Hi.svg', import.meta.url).href,
     CLOSE: new URL('./close.svg', import.meta.url).href,
     HAMBURGER: new URL('./hamburger.svg', import.meta.url).href,
 };
