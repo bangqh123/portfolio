b7ac198139e41216563f4b3ad52aee2534615243
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-11-13 22:04:09 +0700

    edit home page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,16 +1,11 @@
-import "./HomePage.scss"
-
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPortfolio } from "../../redux/slices/portfolioSlices";
 import { RootState, AppDispatch } from "../../redux/store/store";
 
-import HeroSection from "./partials/Heros/Hero"
 import LayoutPage from "../../layouts/LayoutPage/LayoutPage"
-import About from "./partials/Abouts/About"
-import Education from "./partials/Educations/Education"
-import Experience from "./partials/Experiences/Experience"
-import Skill from "./partials/Skill/Skill"
+import HeroSection from "./partials/Heros/Hero"
+import IDo from "./partials/IDo/IDo";
 import { PROPS } from "../../configs/constants/homepage.config"
 
 const HomePage = () => {
@@ -28,12 +23,9 @@ const HomePage = () => {
 
   return (
     <LayoutPage>
-      <HeroSection {...PROPS.HERO} />
       <div className='content-wrapper'>
-        <About  {...PROPS.ABOUTUS}/>
-        <Education {...PROPS.EDUCATION}/>
-        <Experience {...PROPS.EXPERIENCE}/>
-        <Skill {...PROPS.SKILL}/>
+        <HeroSection />
+        <IDo {...PROPS.WHAT}/>
       </div>
     </LayoutPage>
   )
