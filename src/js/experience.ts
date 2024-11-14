217d64f2c5dc4d9ec0604b33a106fdd0458eec4c
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-11-14 14:03:19 +0700

    create experience data

---
+++ src/js/experience.ts
@@ -0,0 +1,14 @@
+import { TExperienceData } from "./../types/experienceType";
+
+const experiencedata: TExperienceData[] = [
+  {
+    school_name: "University of Greenwich (Can Tho)",
+    major: "computer science",
+    desc: "HTML & CSS, Java, PhP, C#",
+    gpa: "3.55/4",
+    start: "Jul 2018",
+    end: "Jul 2023",
+  },
+];
+
+export default experiencedata;
