a84ed6d99693ad0548ffc9eb00e95b381b8007f1
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-11-16 16:38:18 +0700

    create hero contact

---
+++ src/pages/contact/partials/Hero/Hero.tsx
@@ -0,0 +1,64 @@
+import "./Hero.scss"
+import "./Hero.media.scss"
+
+import React, { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux";
+import { RootState, AppDispatch } from "../../../../redux/store/store";
+import SocailButton from "../../../../components/buttons/SocialButtonGroup/SocialButtonGroup";
+import Button from "../../../../components/buttons/Buttons/Buttons";
+
+type THeroProps = {
+    contact: string;
+}
+
+interface THero {
+    img: string;
+    alt: string;
+    desc: string;
+    icons: TIcon[];
+    resume: string ;
+}
+
+interface TIcon {
+    icon: string;
+    color: string;
+    link: string;
+}
+
+const Hero: React.FC<THeroProps> = ( {contact} ) => {
+    const dispatch = useDispatch<AppDispatch>();
+
+  const [isData, setIsData] = useState<THero>();
+
+  const { contactInfo } = useSelector((state: RootState) => state.contact);
+
+  useEffect(() => {
+    setIsData(contactInfo?.[0]?.hero);
+  }, [dispatch, contactInfo]);
+  
+  console.log(isData)
+  
+    return (
+        <div className="contacthero">
+            <div className="contacthero-img">
+                <img src={isData?.img} alt={isData?.alt} />
+            </div>
+            <div className="contacthero-content">
+                <h1 className="contacthero-content-title">{contact}</h1>
+                <p className="contacthero-content-desc">{isData?.desc}</p>
+                <SocailButton
+                    classname="contacthero-content-btn"
+                    data={isData?.icons}
+                />
+                <Button 
+                    newTab={true}
+                    title="See My Resume"
+                    link={isData?.resume}
+                    className="contacthero-content-resume"
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
