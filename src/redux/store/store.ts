5c696e81aff161d46a55ec3a9a384cf96995a543
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-11-17 01:06:40 +0700

    create rudex for education

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import portfolioReducer from "./../slices/portfolioSlices";
 import projectReducer from "./../slices/projectsSlice";
 import contactReducer from "./../slices/contactSlices";
+import educationRecer from "./../slices/educationSlies";
 
 const store = configureStore({
     reducer: {
         portfolio: portfolioReducer,
+        education: educationRecer,
         project: projectReducer,
         contact: contactReducer,
     },
