bfca315ce449c382cd816d335fd19ead1e7486a6
Quach Hai Bang <68978560+bangqh123@users.noreply.github.com>
2024-11-17 20:34:32 +0700

    create hero education

--- src/pages/education/partials/Hero/Hero.tsx
+++ src/pages/education/partials/Hero/Hero.tsx
@@ -4,8 +4,8 @@ import "./Hero.media.scss";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../../redux/store/store";
-// import SocailButton from "../../../../components/buttons/SocialButtonGroup/SocialButtonGroup";
-// import Button from "../../../../components/buttons/Buttons/Buttons";
+import { blueTheme } from "../../../../styles/theme";
+import HeroImg from "./heroimg";
 
 type THeroProps = {
   title: string;
@@ -23,36 +23,32 @@ interface THero {
 const Hero: React.FC<THeroProps> = ({ title }) => {
   const [isData, setIsData] = useState<THero>();
 
-  const { educationInfo } = useSelector(
-    (state: RootState) => state.education
-  );
+  const { educationInfo } = useSelector((state: RootState) => state.education);
 
   useEffect(() => {
-    setIsData(educationInfo?.hero)
+    setIsData(educationInfo?.hero);
   }, [educationInfo]);
 
-  console.log(isData)
-
   return (
-    <div className="contacthero">
-        {title}
-      {/* <div className="contacthero-img">
-        <img src={isData?.img} alt={isData?.alt} />
+    <div className="educationhero">
+      <div className="contacthero-img">
+        <HeroImg theme={blueTheme} />
       </div>
       <div className="contacthero-content">
-        <h1 className="contacthero-content-title">{contact}</h1>
+        <h1 className="contacthero-content-title">{title}</h1>
         <p className="contacthero-content-desc">{isData?.desc}</p>
-        <SocailButton
-          classname="contacthero-content-btn"
-          data={isData?.icons}
-        />
-        <Button
-          newTab={true}
-          title="See My Resume"
-          link={isData?.resume}
-          className="contacthero-content-resume"
-        />
-      </div> */}
+        <div className="contacthero-content-icons">
+          {isData?.icons?.map((iconData, i) => {
+            const IconComponent = iconData.icon;
+            return (
+              <div key={i} className="icon">
+                <img src={IconComponent} alt={iconData.name} />
+                <span>{iconData.name}</span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
